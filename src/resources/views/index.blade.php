564b58ea4872d204da93d940fac9259982614d91
kitano <beiyelinai01@gmail.com>
2024-11-18 15:37:10 +0900

    更新

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -36,7 +36,9 @@
               <input type="text" name="name" placeholder="テスト太郎" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('name')
+              {{$message}}
+              @enderror
             </div>
           </div>
         </div>
@@ -50,7 +52,9 @@
               <input type="email" name="email" placeholder="test@example.com" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('email')
+              {{$message}}
+              @enderror
             </div>
           </div>
         </div>
@@ -64,7 +68,9 @@
               <input type="tel" name="tel" placeholder="09012345678" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('tel')
+              {{$message}}
+              @enderror
             </div>
           </div>
         </div>
